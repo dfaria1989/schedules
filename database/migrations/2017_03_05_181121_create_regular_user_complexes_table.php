2d97233ec9ca55ebd571e82e9bd913e35ece093d
Andre Queiros <Andre Queiros>
2017-03-05 19:14:56 +0000

    Add migrations to create properly tables

---
+++ database/migrations/2017_03_05_181121_create_regular_user_complexes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegularUserComplexesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regular_user_complexes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('complex_id')->unsigned();
+            $table->boolean('accepted_by_admin')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::table('regular_user_complexes', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('complex_id')->references('id')->on('complexes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('regular_user_complexes', function (Blueprint $table) {
+            $table->dropForeign(['user_id', 'complex_id']);
+        });
+
+        Schema::dropIfExists('regular_user_complexes');
+    }
+}
